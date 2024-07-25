7605e466418194657da0d73f395d4db502ac4081
jhj6879 <jo39382355@gmail.com>
2024-07-25 10:06:36 +0900

    수업내용

--- src/main/java/com/myweb/firstboot/controller/BoardController.java
+++ src/main/java/com/myweb/firstboot/controller/BoardController.java
@@ -21,6 +21,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
 
+import com.myweb.firstboot.Search;
 import com.myweb.firstboot.dto.BoardDto;
 import com.myweb.firstboot.dto.FileDto;
 import com.myweb.firstboot.dto.GalaryDto;
@@ -38,12 +39,35 @@ public class BoardController { //게시판 컨트롤러
 	private final BoardService service;
 	private final FileService fileService;
 	
-	//게시판 페이지
+	//게시판 페이지 및 페이징
 	@GetMapping("/board/{board_no}")
 	public String boardPage(Model model, @PathVariable("board_no") int board_no) {
+		Search page = new Search(5, 5);
 		//게시판 리스트
-		List<PostDto> list = service.getPostListByBoard(board_no);
+		List<PostDto> list = service.getPostListByBoard(board_no, page);
 		model.addAttribute("list", list);
+		// 페이징에 대한 것도 같이 가야함
+		model.addAttribute("page",page);
+		//해당 게시글에 대한 번호(정보)
+		BoardDto board = service.getBoard(board_no);
+		model.addAttribute("board", board);
+		//게시판 메뉴
+		List<BoardDto> menu = service.getBoardMenu();
+		model.addAttribute("menu", menu);
+		
+		return "board";
+	}
+	
+	@GetMapping("/board/{board_no}/{page}")
+	public String boardPage(Model model, @PathVariable("board_no") int board_no,
+			@PathVariable("page") int page) {
+		Search search = new Search(5, 5);
+		search.setPage(page);
+		//게시판 리스트
+		List<PostDto> list = service.getPostListByBoard(board_no, search);
+		model.addAttribute("list", list);
+		// 페이징에 대한 것도 같이 가야함
+		model.addAttribute("page",search);
 		//해당 게시글에 대한 번호(정보)
 		BoardDto board = service.getBoard(board_no);
 		model.addAttribute("board", board);
@@ -254,8 +278,37 @@ public class BoardController { //게시판 컨트롤러
 	
 	// 갤러리 페이지
 	@GetMapping("/galary")
-	public String getGalaryList() {
+	public String getGalaryList(Model model) {
+		List<GalaryDto> list =  service.getGalaryList();
+		model.addAttribute("list",list);
 		return "galaryList";
+	}
+	
+	// 갤러리 다운로드
+	@GetMapping("/downloadImg/{id}")
+	public ResponseEntity<Resource> downloadImg(@PathVariable("id") int id){
+		try {
+			ImgmngDto dto = service.downloadImage(id);
+			// 파일이 자장된 위치와 UUID로 저장한 파일 아이디
+			Path filePath = Paths.get(dto.getFile_path()).resolve(dto.getFile_name()).normalize();
+			Resource resource = new UrlResource(filePath.toUri());
+			if(resource.exists()) {
+				String encodeFileName = URLEncoder.encode(dto.getOrg_file_name(), StandardCharsets.UTF_8.toString());  // 텍스트 파일은 오류나서 추가
+				String contentDisposition = "attachment; filename=\"" + encodeFileName + "\"";  // 텍스트 파일은 오류나서 추가
+				// 이부분은 공식으로 생각하고 외우자
+				// http 형식을 맞춰서 다운로드 할때 이름 설정..?
+				return ResponseEntity.ok()
+						.header(HttpHeaders.CONTENT_DISPOSITION, contentDisposition)
+						.header(HttpHeaders.CONTENT_TYPE, "application/octet-stream")
+						.body(resource);
+			}else {
+				// 파일을 찾지 못했을 경우
+				return ResponseEntity.notFound().build();
+			}
+		} catch(IOException e) {
+			e.printStackTrace();
+			return ResponseEntity.notFound().build();
+		}
 	}
 	
 	// 갤러리 페이지
@@ -266,15 +319,18 @@ public class BoardController { //게시판 컨트롤러
 	
 	// 갤러리 업로드
 	@PostMapping("/addGalary")
-	public String galaryPost(Model model, PostDto dto, 
-			@RequestParam(value="file", required=false) MultipartFile[] files) { //파일 가져오는 클래스 적용해서 첨부파일 기능 적용 // 여러 파일 적용하기 위해 배열로 변경
-		
+	public String putGalary(Model model, GalaryDto dto,
+			@RequestParam(value = "file", required = false) MultipartFile[] files,
+			@RequestParam(value = "thumbnail", required = false) String[] thumbnail) {
 		final String path = "D:\\koreait\\JAVA-work\\firstboot\\repository\\";
+		service.addGalary(dto);
 		
 		// 파일 업로드 처리 (!file.isEmpty() 에러 발생해서 변겅)
 		if (files != null) {
-			for (MultipartFile file : files) { // 파일 개수만큼 돌려서 file로 넣어줌
+			for (int i=0; i<files.length; i++) {
 				try {
+					MultipartFile file = files[i];
+//					System.out.println("thumbnail[" + i + "] = " + thumbnail[i]);  //디버깅
 					ImgmngDto imgDto = new ImgmngDto();
 					String org_file_name = file.getOriginalFilename();
 					String file_name = UUID.randomUUID().toString().substring(0, 8) + "_" + org_file_name;
@@ -282,17 +338,53 @@ public class BoardController { //게시판 컨트롤러
 					imgDto.setFile_path(path);
 					imgDto.setOrg_file_name(org_file_name);
 					imgDto.setUserid(dto.getUserid());
+					imgDto.setGalary_id(dto.getId());
+					imgDto.setThumbnail(thumbnail[i]);
 
 					// 실제 파일 저장위치에 파일 업로드
 					file.transferTo(new File(path + file_name)); // 오브젝트로 넘기기
-					fileService.imgUpload(imgDto);
+					service.imgUpload(imgDto);
 				} catch (IOException e) {
 					e.printStackTrace();
 				}
 			}
-			List<GalaryDto> galary_list = fileService.ImgDownloadList(dto.getUserid());
-			model.addAttribute("galary_list", galary_list);
+			List<GalaryDto> list = service.getGalaryList();
+			model.addAttribute("list", list);
 		}
+		
 		return "galaryList";
+	}
+	
+	// 각 이미지마다 내용 보여주기
+	@GetMapping("/imgview/{galary_id}")
+	public String imgviewPost(@PathVariable("galary_id") int galary_id, Model model) {
+		ImgmngDto dto = service.getGalaryId(galary_id);
+
+		// 메뉴
+//		List<GalaryDto> menu = service.getGalaryMenu();
+//		model.addAttribute("menu", menu);
+
+		// 게시판 정보
+		GalaryDto galary = service.getGalary(dto.getGalary_id());
+		model.addAttribute("galary", galary);
+
+		// 파일 취득
+//		List<ImgmngDto> img_list = service.downloadImageList(galary_id);
+//		model.addAttribute("img_list", img_list);
+
+		return "imgviewForm";
+	}
+	
+	@GetMapping("/search/{board_no}")
+	public String searchPost(@RequestParam("keyword") String keyword, Model model,
+			 @PathVariable("board_no") int board_no) {
+		Search page = new Search(5, 5);
+		page.setKeyword(keyword);
+		// 게시판 리스트
+		List<PostDto> list = service.getPostListByKeyword(board_no, page);
+		model.addAttribute("list", list);
+		// 페이징에 대한 것도 같이 가야함
+		model.addAttribute("page", page);
+		return "board";
 	}
 }
